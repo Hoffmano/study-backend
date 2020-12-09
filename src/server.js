0df536aaa048f044939fcf3088d9a211514837ce
Gabriel Hoffman Silva <30511353+Hoffmano@users.noreply.github.com>
2020-12-09 14:06:47 -0300

    feat: adiciona docker compose e variavel de ambiente PORT

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
 import express from "express";
-import router from "./router.js"
-import cors from "cors"
+import router from "./router.js";
+import cors from "cors";
 
 const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(router)
+app.use(cors());
+app.use(express.json());
+app.use(router);
 
-const port = 3333
+const port = process.env.PORT || 3333;
 app.listen(port, () => {
 	console.log(`app listening at http://localhost:${port}`);
 });
